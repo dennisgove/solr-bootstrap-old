67f21bbe4a5f49680658ca57d9db35542faedaf7
Dennis Gove <dpgove@gmail.com>
2016-06-15 16:39:35 -0400

    updated solr

--- bin/env.sh
+++ bin/env.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+
+export INSTALL_HOSTS=()
+export INSTALL_DIRECTORY=
+export SOLR_URL=
+export SOLR_PORTS=()
+export ZOOKEEPER_URL=
+export ZOOKEEPER_PORTS=()
+
+
 export BIN_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
 export BASE_DIR=$BIN_DIR/..
 export DIST_DIR=$BASE_DIR/dist
@@ -18,7 +27,7 @@ export ZOO_DATA_DIR=$DATA_DIR/zookeeper
 #####################################################################
 # Solr specific items
 export SOLR_DIR=$BIN_DIR/solr
-export SOLR_VERSION=6.0.0-SNAPSHOT
+export SOLR_VERSION=7.0.0-SNAPSHOT
 export SOLR_PORT=12345
 export SOLR_LOG_DIR=$LOG_DIR/solr
 export SOLR_LOGS_DIR=$SOLR_LOG_DIR
